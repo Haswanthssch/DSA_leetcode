b6f083e3e943a5aa13a9cf5c7336f4454af08673
Chandu Haswanth Sai Sarath <146622212+Haswanthssch@users.noreply.github.com>
2024-09-09 09:58:00 +0530

    Time: 3 ms (28.52%), Space: 43.3 MB (13.38%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -1,7 +1,8 @@
 class Solution {
     public int countGoodSubstrings(String s) {
         int left = 0, right = 3, c = 0;
-        while(right < s.length()+1){
+        while(right < s.length()+1
+        ){
             Set<Character> set = new HashSet<>();
             for(int i = left; i<right; i++)
                 set.add(s.charAt(i));
