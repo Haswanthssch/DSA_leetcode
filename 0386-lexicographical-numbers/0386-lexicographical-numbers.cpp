42b2754833cfcaa7cfe7e892682e4baf6948d514
Chandu Haswanth Sai Sarath <146622212+Haswanthssch@users.noreply.github.com>
2024-09-21 23:38:08 +0530

    Time: 489 ms (5.05%), Space: 15.7 MB (36.91%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -3,10 +3,10 @@ public:
     std::vector<int> lexicalOrder(int n) {
         std::vector<int> ans;
         for (int i = 1; i <= n; i++) {
-            ans.push_back(i); // Push integers directly
+            ans.push_back(i); 
         }
         std::sort(ans.begin(), ans.end(), [](int a, int b) {
-            return std::to_string(a) < std::to_string(b); // Sort based on string representation
+            return std::to_string(a) < std::to_string(b);
         });
         return ans;
     }
